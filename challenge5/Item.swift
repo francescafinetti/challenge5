3819feeff6b0fa1893298b0b41d38a5374288965
francescafinetti <francy.fine@icloud.com>
2025-02-24 10:11:45 +0100

    Initial Commit

---
+++ challenge5/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  challenge5
+//
+//  Created by Francesca Finetti on 24/02/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
